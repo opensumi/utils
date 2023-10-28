a1e1ae9a2bce958a11c3d78ef8dac6d7e3e6a558
野声 <lijiacheng.ljc@antgroup.com>
2023-10-28 20:54:14 +0800

    test(rpc): add service style

--- packages/simple-rpc/tests/index.test.ts
+++ packages/simple-rpc/tests/index.test.ts
@@ -56,4 +56,40 @@ describe('simple-rpc', () => {
 
     dispose();
   });
+
+  it('has service style', async () => {
+    const { c1, c2, dispose } = createPair();
+    c1.on('short', async (url: string) => {
+      await sleep(100);
+      return url;
+    });
+
+    interface C1 {
+      short(url: string): Promise<string>;
+    }
+
+    interface C2 {
+      add(a: number, b: number): Promise<number>;
+    }
+
+    const client2 = c2.createProxy<C1, C2>({
+      add: async (a: number, b: number) => {
+        return a + b;
+      },
+    });
+
+    const client1 = c1.createProxy<C2, C1>({
+      short: async (url: string) => {
+        return url;
+      },
+    });
+
+    const result = await client1.add(1, 2);
+    expect(result).toBe(3);
+
+    const url = await client2.short('https://www.google.com');
+    expect(url).toBe('https://www.google.com');
+
+    dispose();
+  });
 });
